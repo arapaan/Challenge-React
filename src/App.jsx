209d8c7a80aaba9ad232fccf669ae660e2471db7
Arfan Arrasyid Ramadhan <arfan.arrasyid.78@gmail.com>
2025-09-18 15:01:17 +0700

    add: framework css(Tailwind) & build Login page(not yet finished)

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const name = 'Arapaan';
+// const name = 'Arapaan';
+
+function Title() {
+  return <div className='text-white pt-16'>
+    <h2 className='font-bold'>HELLO THERE</h2>
+    <h3 className='font-semibold'>To access this website, please log in first.</h3>
+  </div>;
+}
+
+function Modal() {
+  return <div className='bg-white rounded-xl border-red-50 ml-60 mr-60 h-96'>
+    <h1>p</h1>
+  </div>;
+}
 
 function App() {  
   return (
-    <div>
-      <h1>Hello {name}! </h1>
-      <p>Belajar JSX</p>
+    <div className='bg-black bg-cover min-h-screen w-full items-center justify-center'>
+      <Title />
+      <Modal />
     </div>
   );
 }
